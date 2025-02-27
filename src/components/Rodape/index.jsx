0ef86381c94f3e25944c1cd6989f1764a955adb8
Melksedeque <freelancer@melksedeque.com.br>
2025-02-27 08:57:26 -0300

    Edição de Rodapé com autoria própria

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -3,7 +3,8 @@ import styles from "./Rodape.module.css";
 export default function Rodape() {
   return (
     <footer className={styles.rodape}>
-      <p>Desenvolvido por Alura.</p>
+      <p>Desenvolvido por <a href="http://github.com/Melksedeque" target="_blank">Melksedeque</a>.</p>
+      <p>Designed by Alura.</p>
     </footer>
   );
 }
