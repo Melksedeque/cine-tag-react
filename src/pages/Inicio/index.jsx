c6f1b183d4e7801d5e604a90875bccc454c56205
Melksedeque Silva <freelancer@melksedeque.com.br>
2025-02-27 10:45:07 -0300

    Chamada de componente de Banner

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,10 +1,12 @@
 import Cabecalho from "@/components/Cabecalho";
 import Rodape from "@/components/Rodape";
+import Banner from "@/components/Banner";
 
 export default function Inicio() {
   return (
     <>
       <Cabecalho />
+      <Banner imagem="Home" />
       <Rodape />
     </>
   );
