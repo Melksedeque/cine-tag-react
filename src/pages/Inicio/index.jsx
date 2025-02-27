b74053828bc70462b514c41df0d753809b34e30c
Melksedeque Silva <freelancer@melksedeque.com.br>
2025-02-27 11:12:07 -0300

    Chamada de componente titulo

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,12 +1,16 @@
 import Cabecalho from "@/components/Cabecalho";
 import Rodape from "@/components/Rodape";
 import Banner from "@/components/Banner";
+import Titulo from "@/components/Titulo";
 
 export default function Inicio() {
   return (
     <>
       <Cabecalho />
       <Banner imagem="Home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes</h1>
+      </Titulo>
       <Rodape />
     </>
   );
