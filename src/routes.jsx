718de134f6d960a4076fcc55dc565400fa27f330
Melksedeque <freelancer@melksedeque.com.br>
2025-02-28 18:26:32 -0300

    Cabeçalho e Rodapé no arquivo de Rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,18 @@
+import Cabecalho from "@/components/Cabecalho";
+import Rodape from "@/components/Rodape";
+import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
+      <Cabecalho />
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/favoritos" element={<Favoritos />} />
       </Routes>
+      <Rodape />
     </BrowserRouter>
   );
 }
