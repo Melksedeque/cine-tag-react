62df639103b295353495d98b8a0eaedea296c090
Melksedeque <freelancer@melksedeque.com.br>
2025-02-28 18:25:47 -0300

    Remoção de Cabeçalho e Rodapé da Home

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,5 +1,3 @@
-import Cabecalho from "@/components/Cabecalho";
-import Rodape from "@/components/Rodape";
 import Banner from "@/components/Banner";
 import Titulo from "@/components/Titulo";
 import Cards from "@/components/Cards";
@@ -7,13 +5,11 @@ import Cards from "@/components/Cards";
 export default function Inicio() {
   return (
     <>
-      <Cabecalho />
       <Banner imagem="Home" />
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes</h1>
       </Titulo>
       <Cards />
-      <Rodape />
     </>
   );
 }
