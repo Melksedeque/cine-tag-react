12695cec79c2149e75e57e01e9e3f92f66252eb1
Melksedeque <freelancer@melksedeque.com.br>
2025-03-01 13:01:17 -0300

    Chamada de contexto que será compartilhado entre as rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,16 +4,19 @@ import Container from "@/components/Container";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FavoritosProvider from "contexts/Favoritos";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Cabecalho />
       <Container>
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/favoritos" element={<Favoritos />} />
-        </Routes>
+        <FavoritosProvider>
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/favoritos" element={<Favoritos />} />
+          </Routes>
+        </FavoritosProvider>
       </Container>
       <Rodape />
     </BrowserRouter>
