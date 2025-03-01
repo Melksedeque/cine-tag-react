a0116635a457a8bb7bee922f4f0b1af88490bfbc
Melksedeque <freelancer@melksedeque.com.br>
2025-03-01 14:26:31 -0300

    Mudança de estrutura trazendo o map para dentro da página ao inves do componente; Refatorando.

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,6 +1,8 @@
 import Banner from "@/components/Banner";
 import Titulo from "@/components/Titulo";
 import Cards from "@/components/Cards";
+import Card from "@/components/Cards/Card";
+import videos from "@/json/db.json";
 
 export default function Inicio() {
   return (
@@ -9,7 +11,11 @@ export default function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes</h1>
       </Titulo>
-      <Cards />
+      <Cards>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </Cards>
     </>
   );
 }
