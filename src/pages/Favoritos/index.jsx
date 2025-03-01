8b75b4fefaa60eec5fea6d1afc33409ceba14d84
Melksedeque <freelancer@melksedeque.com.br>
2025-03-01 14:27:09 -0300

    Criação de página de favoritos com map do context; Ainda com erro, precisa analisar o console para corrigir;

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,15 +1,22 @@
 import Banner from "@/components/Banner";
 import Titulo from "@/components/Titulo";
 import Cards from "@/components/Cards";
+import Card from "@/components/Cards/Card";
+import { useFavoritosContext } from "@/contexts/Favoritos";
 
 export default function Favoritos() {
+  const { favoritos } = useFavoritosContext();
   return (
     <>
       <Banner imagem="Favoritos" />
       <Titulo>
         <h1>Meus Favoritos</h1>
       </Titulo>
-      <Cards />
+      <Cards>
+        {favoritos.map((fav) => {
+          return <Card {...fav} key={fav.id} />;
+        })}
+      </Cards>
     </>
   );
 }
