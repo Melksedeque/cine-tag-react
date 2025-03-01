46427bc1bb11bd2a9a63ee79bb176d386712bf37
Melksedeque <freelancer@melksedeque.com.br>
2025-03-01 14:25:57 -0300

    Refatorando cards com children

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,13 +1,5 @@
-import Card from "./Card";
 import styles from "./Cards.module.css";
-import videos from "@/json/db.json";
 
-export default function Cards() {
-  return (
-    <section className={styles.cardsContainer}>
-      {videos.map((video) => {
-        return <Card {...video} key={video.id} />;
-      })}
-    </section>
-  );
+export default function Cards({ children }) {
+  return <section className={styles.cardsContainer}>{children}</section>;
 }
