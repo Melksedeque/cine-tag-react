cb3ce2d0997e1840026f3b6732f1962a887abde0
Melksedeque <freelancer@melksedeque.com.br>
2025-03-01 14:03:36 -0300

    Importar provider de Favoritos

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,10 @@
 import Cabecalho from "@/components/Cabecalho";
 import Rodape from "@/components/Rodape";
 import Container from "@/components/Container";
+import FavoritosProvider from "@/contexts/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FavoritosProvider from "contexts/Favoritos";
 
 export default function AppRoutes() {
   return (
