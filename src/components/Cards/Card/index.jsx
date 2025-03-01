9d34ac72d8cb9f1ded792b8961015a7dcf56fd1c
Melksedeque <freelancer@melksedeque.com.br>
2025-03-01 14:17:26 -0300

    Criação de variaveis de icone diferente para favorito ou não

--- src/components/Cards/Card/index.jsx
+++ src/components/Cards/Card/index.jsx
@@ -1,17 +1,19 @@
 import { useFavoritosContext } from "@/contexts/Favoritos";
 import styles from "./Card.module.css";
 import iconeFavoritar from "./favoritar.png";
-// import iconeDesfavoritar from "./desfavoritar.png";
+import iconeDesfavoritar from "./desfavoritar.png";
 
 export default function Card({ id, titulo, capa }) {
   const { favorito, manipularFavorito } = useFavoritosContext();
-  
+  const ehFavorito = favorito.some((fav) => fav.id === id);
+  const icone = ehFavorito ? iconeDesfavoritar : iconeFavoritar;
+
   return (
     <figure key={id} className={styles.card}>
       <img src={capa} alt={titulo} className={styles.capa} />
       <figcaption className={styles.titulo}>{titulo}</figcaption>
       <img
-        src={iconeFavoritar}
+        src={icone}
         alt="Favoritar filme"
         className={styles.favoritar}
         onClick={() => {
