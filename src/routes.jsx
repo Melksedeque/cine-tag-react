928bc7e52f1a15fbdb7dc1a3748b94e485b595cf
Melksedeque <freelancer@melksedeque.com.br>
2025-03-03 13:05:31 -0300

    Criação de rota para Player do video

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import Container from "@/components/Container";
 import FavoritosProvider from "@/contexts/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
+import Player from "pages/Player";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function AppRoutes() {
@@ -15,6 +16,7 @@ export default function AppRoutes() {
           <Routes>
             <Route path="/" element={<Inicio />} />
             <Route path="/favoritos" element={<Favoritos />} />
+            <Route path="/player/:id" element={<Player />} />
           </Routes>
         </FavoritosProvider>
       </Container>
