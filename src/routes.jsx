3a70fb8c08c074adc5cf96b767fec534761250cc
Melksedeque <freelancer@melksedeque.com.br>
2025-03-03 13:35:51 -0300

    Criação de rota universal para 404

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,6 +5,7 @@ import FavoritosProvider from "@/contexts/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import Player from "pages/Player";
+import NaoEncontrada from "pages/NaoEncontrada";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function AppRoutes() {
@@ -17,6 +18,7 @@ export default function AppRoutes() {
             <Route path="/" element={<Inicio />} />
             <Route path="/favoritos" element={<Favoritos />} />
             <Route path="/player/:id" element={<Player />} />
+            <Route path="*" element={<NaoEncontrada />} />
           </Routes>
         </FavoritosProvider>
       </Container>
