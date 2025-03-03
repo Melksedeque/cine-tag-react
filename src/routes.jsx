f1b6bc43d64eca55e5cc3667394c702bce06d0df
Melksedeque <freelancer@melksedeque.com.br>
2025-03-03 13:46:27 -0300

    Aninhamento de rotas dentro da página base

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,28 +1,21 @@
-import Cabecalho from "@/components/Cabecalho";
-import Rodape from "@/components/Rodape";
-import Container from "@/components/Container";
-import FavoritosProvider from "@/contexts/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import Player from "pages/Player";
 import NaoEncontrada from "pages/NaoEncontrada";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PaginaBase from "pages/PaginaBase";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <Cabecalho />
-      <Container>
-        <FavoritosProvider>
-          <Routes>
-            <Route path="/" element={<Inicio />} />
-            <Route path="/favoritos" element={<Favoritos />} />
-            <Route path="/player/:id" element={<Player />} />
-            <Route path="*" element={<NaoEncontrada />} />
-          </Routes>
-        </FavoritosProvider>
-      </Container>
-      <Rodape />
+      <Routes>
+        <Route path="/" element={<PaginaBase />}>
+          <Route index element={<Inicio />} />
+          <Route path="favoritos" element={<Favoritos />} />
+          <Route path="player/:id" element={<Player />} />
+          <Route path="*" element={<NaoEncontrada />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
